a0015b646ee2abc90c5274afd0b5a180406e47bb
rioobod <akanbilola@gmail.com>
2022-03-05 10:54:16 -0600

    updated files to create web server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const { animals } = require('./data/animals');
 const express = require('express');
-const res = require('express/lib/response');
+const { animals } = require('./data/animals');
+//const res = require('express/lib/response');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -36,12 +36,26 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
   }
 
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
+
   app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
       results = filterByQuery(req.query, results);
     }
     res.json(results);
+  });
+
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+    res.send(404);
+    }
   });
 
   app.listen(PORT, () => {
