22b1718d47aefc15b50f389e7985e19f6729e05d
rioobod <akanbilola@gmail.com>
2022-03-05 10:19:16 -0600

    add heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const { animals } = require('./data/animals');
 const express = require('express');
 const res = require('express/lib/response');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -43,6 +44,6 @@ function filterByQuery(query, animalsArray) {
     res.json(results);
   });
 
-app.listen(3001, () => {
-    console.log('API server now on port 3001!');
-});
+  app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
