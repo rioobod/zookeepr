580456ee64139ad5da53830366314c7009d3f60d
rioobod <akanbilola@gmail.com>
2022-03-06 14:21:35 -0600

    get request for data

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -90,12 +91,7 @@ function filterByQuery(query, animalsArray) {
     }
   });
 
- // app.post('/api/animals', (req, res) => {
- // console.log(req.body);
-//  res.json(req.body);
-//});
-
-app.post('/api/animals', (req, res) => {
+  app.post('/api/animals', (req, res) => {
     // set id based on what the next index of the array will be
     req.body.id = animals.length.toString();
   
@@ -107,6 +103,29 @@ app.post('/api/animals', (req, res) => {
       res.json(animal);
     }
   });
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+  app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+  app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+  
+ // app.post('/api/animals', (req, res) => {
+ // console.log(req.body);
+//  res.json(req.body);
+//});
+
 
   app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
